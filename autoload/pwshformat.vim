9a236cc1f754516a678ade0b622626c096410352
JayDoubleu <jaroslaw.walaszek@gmail.com>
2019-07-06 23:25:24 +0100

    Set default shell

--- autoload/pwshformat.vim
+++ autoload/pwshformat.vim
@@ -4,6 +4,7 @@ else
   let g:loaded_pwsh_formatter = 'yes'
 endif
 
+setlocal shell=sh
 let plugindir = expand('<sfile>:p:h:h')
 let absPath  = expand(plugindir . '/plugin/format.ps1')
 let s:cmd = expand(':%!pwsh -noprofile -command ''$Input | ' . absPath . '''')
--- plugin/format.ps1
+++ plugin/format.ps1
@@ -1,101 +1,101 @@
 # Big thanks to markekraus #powershell@freenode for stdin solution.
 [cmdletbinding()]
 param(
-  [Parameter(ValueFromPipeline)]
-  [String]
-  $InputObject
+    [Parameter(ValueFromPipeline)]
+    [String]
+    $InputObject
 )
 
 begin {
-  $StringBuilder = [System.Text.StringBuilder]::new()
+    $StringBuilder = [System.Text.StringBuilder]::new()
 }
 process {
-  $StringBuilder.AppendLine($InputObject) | Out-Null
+    $StringBuilder.AppendLine($InputObject) | Out-Null
 }
 
 end {
-  $settings = @{
-    IncludeRules = @(
-      "PSPlaceOpenBrace",
-      "PSPlaceCloseBrace",
-      "PSUseConsistentWhitespace",
-      "PSUseConsistentIndentation",
-      "PSAvoidDefaultValueForMandatoryParameter",
-      "PSAvoidDefaultValueSwitchParameter",
-      "PSAvoidGlobalAliases",
-      "PSAvoidGlobalFunctions",
-      "PSAvoidGlobalVars",
-      "PSAvoidInvokingEmptyMembers",
-      "PSAvoidShouldContinueWithoutForce",
-      "PSAvoidUsingCmdletAliases",
-      "PSAvoidUsingComputerNameHardcoded",
-      "PSAvoidUsingConvertToSecureStringWithPlainText",
-      "PSAvoidUsingDeprecatedManifestFields",
-      "PSAvoidUsingEmptyCatchBlock",
-      "PSAvoidUsingInvokeExpression",
-      "PSAvoidUsingPlainTextForPassword",
-      "PSAvoidUsingPositionalParameters",
-      "PSAvoidUsingUserNameAndPassWordParams",
-      "PSAvoidUsingWMICmdlet",
-      "PSAvoidUsingWriteHost",
-      "PSMisleadingBacktick",
-      "PSMissingModuleManifestField",
-      "PSPossibleIncorrectComparisonWithNull",
-      "PSProvideCommentHelp",
-      "PSReservedCmdletChar",
-      "PSReservedParams",
-      "PSShouldProcess",
-      "PSUseApprovedVerbs",
-      "PSUseBOMForUnicodeEncodedFile",
-      "PSUseCmdletCorrectly",
-      "PSUseDeclaredVarsMoreThanAssignments",
-      "PSUseLiteralInitializerForHashtable",
-      "PSUseOutputTypeCorrectly",
-      "PSUsePSCredentialType",
-      "PSUseShouldProcessForStateChangingFunctions",
-      "PSUseSingularNouns",
-      "PSUseSupportsShouldProcess",
-      "PSUseToExportFieldsInManifest"
-    )
+    $settings = @{
+        IncludeRules = @(
+            "PSPlaceOpenBrace",
+            "PSPlaceCloseBrace",
+            "PSUseConsistentWhitespace",
+            "PSUseConsistentIndentation",
+            "PSAvoidDefaultValueForMandatoryParameter",
+            "PSAvoidDefaultValueSwitchParameter",
+            "PSAvoidGlobalAliases",
+            "PSAvoidGlobalFunctions",
+            "PSAvoidGlobalVars",
+            "PSAvoidInvokingEmptyMembers",
+            "PSAvoidShouldContinueWithoutForce",
+            "PSAvoidUsingCmdletAliases",
+            "PSAvoidUsingComputerNameHardcoded",
+            "PSAvoidUsingConvertToSecureStringWithPlainText",
+            "PSAvoidUsingDeprecatedManifestFields",
+            "PSAvoidUsingEmptyCatchBlock",
+            "PSAvoidUsingInvokeExpression",
+            "PSAvoidUsingPlainTextForPassword",
+            "PSAvoidUsingPositionalParameters",
+            "PSAvoidUsingUserNameAndPassWordParams",
+            "PSAvoidUsingWMICmdlet",
+            "PSAvoidUsingWriteHost",
+            "PSMisleadingBacktick",
+            "PSMissingModuleManifestField",
+            "PSPossibleIncorrectComparisonWithNull",
+            "PSProvideCommentHelp",
+            "PSReservedCmdletChar",
+            "PSReservedParams",
+            "PSShouldProcess",
+            "PSUseApprovedVerbs",
+            "PSUseBOMForUnicodeEncodedFile",
+            "PSUseCmdletCorrectly",
+            "PSUseDeclaredVarsMoreThanAssignments",
+            "PSUseLiteralInitializerForHashtable",
+            "PSUseOutputTypeCorrectly",
+            "PSUsePSCredentialType",
+            "PSUseShouldProcessForStateChangingFunctions",
+            "PSUseSingularNouns",
+            "PSUseSupportsShouldProcess",
+            "PSUseToExportFieldsInManifest"
+        )
 
-    Rules = @{
-      PSPlaceOpenBrace = @{
-        Enable = $true
-        OnSameLine = $true
-        NewLineAfter = $true
-        IgnoreOneLineBlock = $true
-      }
-      PSPlaceCloseBrace = @{
-        Enable = $true
-        NewLineAfter = $true
-        IgnoreOneLineBlock = $true
-        NoEmptyLineBefore = $false
-      }
-      PSUseConsistentIndentation = @{
-        Enable = $true
-        Kind = "space"
-        IndentationSize = 4
-      }
-      PSUseConsistentWhitespace = @{
-        Enable = $true
-        CheckOpenBrace = $true
-        CheckOpenParen = $true
-        CheckOperator = $true
-        CheckSeparator = $true
-      }
-      PSAvoidUsingCmdletAliases = @{
-        Whitelist = @("%", "?", "select", "sort", "group")
-      }
-      PSProvideCommentHelp = @{
-        Enable = $true
-        ExportedOnly = $false
-        BlockComment = $true
-        VSCodeSnippetCorrection = $true
-        Placement = "begin"
-      }
+        Rules        = @{
+            PSPlaceOpenBrace           = @{
+                Enable             = $true
+                OnSameLine         = $true
+                NewLineAfter       = $true
+                IgnoreOneLineBlock = $true
+            }
+            PSPlaceCloseBrace          = @{
+                Enable             = $true
+                NewLineAfter       = $true
+                IgnoreOneLineBlock = $true
+                NoEmptyLineBefore  = $false
+            }
+            PSUseConsistentIndentation = @{
+                Enable          = $true
+                Kind            = "space"
+                IndentationSize = 4
+            }
+            PSUseConsistentWhitespace  = @{
+                Enable         = $true
+                CheckOpenBrace = $true
+                CheckOpenParen = $true
+                CheckOperator  = $true
+                CheckSeparator = $true
+            }
+            PSAvoidUsingCmdletAliases  = @{
+                Whitelist = @("%", "?", "select", "sort", "group")
+            }
+            PSProvideCommentHelp       = @{
+                Enable                  = $true
+                ExportedOnly            = $false
+                BlockComment            = $true
+                VSCodeSnippetCorrection = $true
+                Placement               = "begin"
+            }
+        }
     }
-  }
-  $PipelineContent = $StringBuilder.ToString().TrimEnd()
-  #Invoke-Formatter -Settings $settings -ScriptDefinition $PipelineContent
-  Invoke-Formatter -ScriptDefinition $PipelineContent
+    $PipelineContent = $StringBuilder.ToString().TrimEnd()
+    #Invoke-Formatter -Settings $settings -ScriptDefinition $PipelineContent
+    Invoke-Formatter -ScriptDefinition $PipelineContent
 }
